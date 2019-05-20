e9849ad26224393bdb0bb1922534275f1623d853
shintyabachan <icinbachan15@gmail.com>
2019-05-20 12:43:28 +0700

    Updates

---
+++ precache-manifest.7746209e247a4b4fda2817183c86a421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b51486155439fbf148f8bd9da09e18b",
+    "url": "/micinTa/index.html"
+  },
+  {
+    "revision": "999a7471b4dfcfe8e723",
+    "url": "/micinTa/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "c7835f3d81dabf380888",
+    "url": "/micinTa/static/css/main.5a9ff18f.chunk.css"
+  },
+  {
+    "revision": "999a7471b4dfcfe8e723",
+    "url": "/micinTa/static/js/2.81262243.chunk.js"
+  },
+  {
+    "revision": "c7835f3d81dabf380888",
+    "url": "/micinTa/static/js/main.c9f19c6b.chunk.js"
+  },
+  {
+    "revision": "12be8e4642f977b9009d",
+    "url": "/micinTa/static/js/runtime~main.f9f9cb49.js"
+  }
+]);
