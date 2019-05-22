55aeddc19a070e5b700ffee41066ca89a1654d9a
shintyabachan <icinbachan15@gmail.com>
2019-05-22 23:48:59 +0700

    final

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 
+import { Lines } from 'react-preloaders';
+ 
+ReactDOM.render(
+    <Lines color={'#f7f7f7'} 
+    bgColor={'#222'} 
+    time={1000}
+    />,
+    document.getElementById('preloader')
+);
+
+
 ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
